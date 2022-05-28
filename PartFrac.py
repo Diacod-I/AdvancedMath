7576cfce755f541412676d8356ce73510e1af232
Luci <98768417+Diacod-I@users.noreply.github.com>
2022-05-28 20:24:03 +0530

    Update PartFrac.py

--- PartFrac.py
+++ PartFrac.py
@@ -1,14 +1,21 @@
+from re import search
 from sympy import *
-from sympy.integrals.transforms import inverse_laplace_transform
-from sympy import exp, Symbol
-from sympy.abc import s
+from sympy.integrals.transforms import inverse_laplace_transform,laplace_transform
+from sympy import exp
+from sympy.abc import s,t
 s,t = symbols('s t')
+def eLAPorINV(value):
+    if (search("s",str(value))):
+        tvalue=inverse_laplace_transform(value,s,t)
+        print("Inverse Laplace:",tvalue)
+    elif (search("t",str(value))):
+        svalue=laplace_transform(value,t,s)
+        print("Laplace:",svalue)    
 try :
-    value = apart(input("Apply the function in terms of s and t (use brackets wherever necessary):"))
-    svalue=inverse_laplace_transform(value,s,t)
-except EOFError:
-        print("Oii, write it properly..")
+    value = (input("Apply the function in terms of s or t (use brackets wherever necessary):"))
+    print("Partial fraction:",apart(value))
+    eLAPorINV(value)
 except KeyboardInterrupt: 
-    print("Oof.. Okay fine I guess..")
-print("Partial fraction:",value)
-print("Inverse Laplace:",svalue)
+    print("\n\n\nOof.. Okay fine I guess..")
+except:
+    print("An exception occured while trying to find apart()")
